2673141b6047958024c2a2594e50821d2ba5c2f2
imshreevyas <shreevyas65@gmail.com>
2025-02-02 22:32:56 +0530

    Added Admin files

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'admin' => [ // New admin guard
+            'driver' => 'session',
+            'provider' => 'admins', // Use the admins model provider
+        ],
     ],
 
     /*
@@ -63,6 +67,10 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
+        ],
+        'admins' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Admin::class,
         ],
 
         // 'users' => [
